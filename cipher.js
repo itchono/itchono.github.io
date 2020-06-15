c4917a19f6f8aa41d86bafdf7c78ea4d0e7d85a9
Mingde Yin <54449457+itchono@users.noreply.github.com>
2020-06-15 14:37:55 -0400

    Cipher up and running

--- cipher.js
+++ cipher.js
@@ -1,22 +1,30 @@
 // Cipher tool
 
-function encrypt() {
-    var cipherkeybox = document.getElementById("cipherkey")
-    var ciphertextbox = document.getElementById("ciphertext")
-
-    key = cipherkeybox.value
-    plaintext = ciphertextbox.value
-
+function encrypt(plaintext) {
     var items = plaintext.split(" ")
     var reversed = []
 
     for (i = 0; i < items.length; i++){
         reversed.push(items[i].split("").reverse().join(""))
     }
-    
-    ciphertextbox.value = reversed.join(" ")
-
-    copyToClipboard(ciphertextbox.value)
-
-    
+    return reversed.join(" ")
 }
+
+async function pasteencrypt() {
+    const text = await navigator.clipboard.readText();
+    copyToClipboard(encrypt(text))
+}
+
+function handleencrypt() {
+    var ciphertextbox = document.getElementById("ciphertext")
+    plaintext = ciphertextbox.value
+    ciphertextbox.value = encrypt(plaintext)
+    copyToClipboard(ciphertextbox.value) 
+}
+
+/*
+async function paste(input) {
+    const text = await navigator.clipboard.readText();
+    input.value = text;
+}
+*/
